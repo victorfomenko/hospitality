6819e1462eddab5abaa694a86949694c6bb15886
Victor Fomenko <viktor.fomenko@iqoption.com>
2019-05-28 19:13:01 +0300

    start create chatbot

--- src/modules/chatbot/pages/ChatbotPage/ChatbotPage.tsx
+++ src/modules/chatbot/pages/ChatbotPage/ChatbotPage.tsx
@@ -1,110 +1,28 @@
+import storage from 'local-storage-fallback';
 import React from 'react';
+import Iframe from 'react-iframe';
 
-class ChatbotPage extends React.PureComponent {
-  public render() {
-    return (
-      <div>
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-        ChatbotPage
-        <br />
-      </div>
-    );
-  }
-}
+const ChatbotPage = () => {
+  React.useEffect(() => {
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  });
+
+  const handleMessage = ({ data }: { data: JSON }) => {
+    if (data && typeof data === 'string') {
+      const { type, data: parseData } = JSON.parse(data);
+      storage.setItem(type, parseData);
+    }
+  };
+
+  return (
+    <Iframe
+      url="http://demo-ui.sofiapulse.com/flow/5cc0971e3b38664237b6a077?postMessageUrl=http://localhost:3000"
+      frameBorder={0}
+      width="100%"
+      height="100%"
+    />
+  );
+};
 
 export default ChatbotPage;
