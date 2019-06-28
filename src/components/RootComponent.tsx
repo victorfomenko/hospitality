1bf5c33640c834ef429ac97ebaf40b28a8297f43
Victor Fomenko <victorfomenko@me.com>
2019-06-28 08:42:53 +0300

    add GA

--- src/components/RootComponent.tsx
+++ src/components/RootComponent.tsx
@@ -4,7 +4,15 @@ import store from '../config/store';
 import RootRouter from './RootRouter';
 import ThemeProviders from './ThemeProviders';
 
+const GA_DISABLE_KEY: string = `ga-disable-${
+  process.env.REACT_APP_GA_MEASUREMENT_ID
+}`;
+
 const App: React.FC = () => {
+  // disable GA if not production
+  if (process.env.NODE_ENV !== 'production') {
+    (window as any)[GA_DISABLE_KEY] = true;
+  }
   return (
     <Provider store={store}>
       <ThemeProviders>
