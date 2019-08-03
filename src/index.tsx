5df239e96e0764776f46915ea19881678fbbc736
Sukhrob Rakhimov <suhrob@gmail.com>
2019-08-03 17:41:00 +0300

    add polyfills

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import * as serviceWorker from './serviceWorker';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/RootComponent';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
